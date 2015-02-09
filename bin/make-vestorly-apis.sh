074ac83131a93c909a3d9a8e3098df234c9e9024
Joseph Misiti <joseph.misiti@gmail.com>
2015-02-08 21:20:55 -0500

    removed demo scripts, added new generation scripts

---
+++ bin/make-vestorly-apis.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+java -DdebugSwagger -cp ./target/*:./target/lib/* com.wordnik.swagger.codegen.Codegen -i http://developers.vestorly.com/api-docs -l python -o vestorly/python
+java -DdebugSwagger -cp ./target/*:./target/lib/* com.wordnik.swagger.codegen.Codegen -i http://developers.vestorly.com/api-docs -l objc -o vestorly/objectivec
+java -DdebugSwagger -cp ./target/*:./target/lib/* com.wordnik.swagger.codegen.Codegen -i http://developers.vestorly.com/api-docs -l php -o vestorly/php
+java -DdebugSwagger -cp ./target/*:./target/lib/* com.wordnik.swagger.codegen.Codegen -i http://developers.vestorly.com/api-docs -l java -o vestorly/java
+java -DdebugSwagger -cp ./target/*:./target/lib/* com.wordnik.swagger.codegen.Codegen -i http://developers.vestorly.com/api-docs -l scala -o vestorly/scala
+java -DdebugSwagger -cp ./target/*:./target/lib/* com.wordnik.swagger.codegen.Codegen -i http://developers.vestorly.com/api-docs -l android -o vestorly/android
+java -DdebugSwagger -cp ./target/*:./target/lib/* com.wordnik.swagger.codegen.Codegen -i http://developers.vestorly.com/api-docs -l nodejs -o vestorly/javascript
