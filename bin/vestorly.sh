c12ea85b64fdcb5d62c1ea1b2ffc37aaaeb60593
Joseph Misiti <joseph.misiti@gmail.com>
2015-04-13 17:55:01 -0400

    SwaggerPetstore -> vestorly

---
+++ bin/vestorly.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+#
+# This script will build all the clients
+#
+
+declare -a arr=('android'  'java'  'jaxrs' 'nodejs' 'objc' 'scalatra' 'scala' 'dynamic-html' 'html' 'swagger' 'tizen' 'php'  'python')
+declare -a arr=('python')
+
+# for i in "${arr[@]}"
+# do
+# 	java -jar modules/swagger-codegen-distribution/target/swagger-codegen-distribution-2.1.1-M1.jar  \
+# 		 -i http://developers.vestorly.com/v1/api-docs  \
+# 		 -l $i   -o /Users/josephmisiti/mathandpencil/projects/vestorly/clients/v1/$i
+#
+# done
+#
+#
+# for i in "${arr[@]}"
+# do
+# 	java -jar modules/swagger-codegen-distribution/target/swagger-codegen-distribution-2.1.1-M1.jar  \
+# 		 -i http://developers.vestorly.com/v2/api-docs  \
+# 		 -l $i   -o /Users/josephmisiti/mathandpencil/projects/vestorly/clients/v2/$i
+#
+# done
+
+
+# for i in "${arr[@]}"
+# do
+# 	java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate  \
+# 		 -i http://developers.vestorly.com/v1/api-docs  \
+# 		 -l $i   -o /Users/josephmisiti/mathandpencil/projects/vestorly/clients/v1/$i
+#
+# done
+
+
+for i in "${arr[@]}"
+do
+	java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate  \
+		 -i http://developers.vestorly.com/v2/api-docs  \
+		 -l $i   -o /Users/josephmisiti/mathandpencil/projects/vestorly/clients/v2/$i
+
+done
+
