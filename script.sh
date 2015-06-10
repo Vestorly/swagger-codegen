79cebd6660b0bb369904ac1312a7068afea6ba17
Fahim Abouelfadl <fahim.afadl@gmail.com>
2015-06-10 01:58:55 -0400

    Modify CSharp package name and namespace; Update script

--- script.sh
+++ script.sh
@@ -4,12 +4,12 @@
 # This script will build all the clients
 #
 
-declare -a arr=('ruby' 'android'  'java'  'jaxrs' 'nodejs' 'objc' 'scalatra' 'scala' 'dynamic-html' 'html' 'swagger' 'tizen' 'php'  'python')
+declare -a arr=('android'  'csharp' 'dynamic-html' 'html' 'java'  'jaxrs' 'nodejs' 'objc' 'php' 'python' 'ruby' 'scala' 'scalatra' 'swagger' 'tizen')
 
 for i in "${arr[@]}"
 do
     java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate  \
          -i http://127.0.0.1:3000/apidocs  \
-         -l $i   -o ~/Clients/v3/$i
+         -l $i   -o ~/Clients/v2/$i
 
 done
